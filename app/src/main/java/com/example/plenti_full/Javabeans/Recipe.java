05649322161212989912fdea2bd8378327bf84d1
Owen Bick <w0726133@myscc.ca>
2020-03-09 22:40:51 -0400

    Created recycler view using GridLayoutManager, and a recipe item view. Fixed minor issues causing application to crash upon launch.

--- app/src/main/java/com/example/plenti_full/Javabeans/Recipe.java
+++ app/src/main/java/com/example/plenti_full/Javabeans/Recipe.java
@@ -3,7 +3,30 @@ package com.example.plenti_full.Javabeans;
 public class Recipe {
 
     private String name;
-    private int image;
     private String description;
-    private String link;
+
+    private int image;
+
+    public Recipe(String name, String description, int image) {
+        this.name = name;
+        this.description = description;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 }
